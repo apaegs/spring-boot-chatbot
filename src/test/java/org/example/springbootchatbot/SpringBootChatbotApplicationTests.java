c93a027edc66b4769796f6cf1cc8add3216d8d78
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-05-04 16:30:39 +0200

    git init

---
+++ src/test/java/org/example/springbootchatbot/SpringBootChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
