e7b03d68a53b37b1bc0dce165985cd15dfcb7ebc
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-05-11 10:31:09 +0200

    Set up test profile for application tests and clean up extra whitespace in index.html.

--- src/test/java/org/example/springbootchatbot/SpringBootChatbotApplicationTests.java
+++ src/test/java/org/example/springbootchatbot/SpringBootChatbotApplicationTests.java
@@ -2,10 +2,12 @@ package org.example.springbootchatbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.example.springbootchatbot.client.AiClient;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootChatbotApplicationTests {
 
     @MockitoBean
